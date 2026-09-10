ea804dbc32a9e566b141492821b2fcad5dcbdb97
jefftardif <jeff@grdf.ca>
2026-09-09 20:55:36 -0400

    Skip planar reflection callback on world map

--- Assets/Houidisoft technology/ESSW EASY SETUP STYLIZED WATER 2.0/Scripts/PlanarReflections.cs
+++ Assets/Houidisoft technology/ESSW EASY SETUP STYLIZED WATER 2.0/Scripts/PlanarReflections.cs
@@ -149,7 +149,7 @@ namespace ESSW.Editorcontroller
 
         private void PreRender(ScriptableRenderContext context, Camera cam)
         {
-            if (Application.isPlaying && SceneManager.GetActiveScene().name.Contains("WorldMap")) return;
+            if (SceneManager.GetActiveScene().name.Contains("WorldMap")) return;
             if (ShouldSkipCamera(cam)) return;
             if (_probe0 == null) InitializeProbe();
 
